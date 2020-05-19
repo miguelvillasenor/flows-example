71c8767c8b87a4d13b7826077f82970cabe81591
miguel <miguel.villasenor@outlook.com>
2020-05-18 21:38:33 -0500

    Add channel for listener

--- app/src/main/java/dev/mvillasenor/realtimestreams/ext/ui.kt
+++ app/src/main/java/dev/mvillasenor/realtimestreams/ext/ui.kt
@@ -1,10 +1,27 @@
 package dev.mvillasenor.realtimestreams.ext
 
 import android.widget.ImageView
+import android.widget.SearchView
 import com.squareup.picasso.Picasso
+import kotlinx.coroutines.channels.Channel
 
 fun ImageView.loadImage(url: String) {
     Picasso.get().cancelRequest(this)
     Picasso.get().load(url)
         .into(this)
 }
+
+val SearchView.queriesChannel: Channel<String>
+    get() {
+        val channel = Channel<String>()
+        this.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean = false
+            override fun onQueryTextChange(newText: String?): Boolean {
+                newText?.let {
+                    channel.offer(newText)
+                }
+                return true
+            }
+        })
+        return channel
+    }
